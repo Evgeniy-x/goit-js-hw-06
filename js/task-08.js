ca4c3e40bc4f3aac632db37063f988e79922f19e
Evgeniy <yaremenko.x@gmail.com>
2023-03-20 16:41:40 +0200

    done hw-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
 
+
+form.addEventListener('submit', onFormSubmit);
+
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const { email, password }  = event.currentTarget.elements;
+
+    if(email.value === '' || password.value === '') {
+        return alert('Заповніть всі поля!');
+    }
+  
+  const obj = {
+      email: email.value,
+      password:password.value
+    }
+  console.log(obj)
+  // console.log(`Login: ${email.value}, Password: ${password.value}`);
+  
+  event.currentTarget.reset();
+}; 
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body')
+const buttonChange = document.querySelector('.change-color');
+const table = document.querySelector('.color');
+
+buttonChange.addEventListener('click', ()=> {
+
+  const randomColor = getRandomHexColor();
+  table.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
+});
